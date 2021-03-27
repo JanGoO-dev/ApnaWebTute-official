db047569e18048cf2e612b57cc99eaafc4fa7bcf
JanGoO-dev <55313789+JanGoO-dev@users.noreply.github.com>
2021-03-27 16:12:02 +0500

    Added Continouse Integration for pushing code to github and then deploying the production version of website to firebase hosting after building the project using vite

--- github-ci.bat
+++ github-ci.bat
@@ -1,4 +1,37 @@
+@echo off & echo Step 1: PUSH CHANGES TO THE GITHUB REPOSITORY
+
+pause
+
+cls
+
 git add .
 @echo off & set /p msg=Enter commit message:  
 git commit -m "%msg%" & git push origin master & echo %msg%
-@echo off & echo "%msg%"
+
+cls
+
+@echo off & echo CHANGES PUSHED SUCCESSFULLY TO THE GITHUB REPOSITORY
+
+pause
+
+cls
+
+@echo off & echo Step 2: BUILD THE PROJECT FOR PRODUCTION
+
+pause
+
+cls
+
+npm run build
+
+cls
+
+@echo of & echo PRODUCTION BUILD SUCCESSFUL
+
+pause
+
+cls
+
+@echo off & echo Step 3: DEPLOYING THE PRODUCTION VERSION ON FIREBASE HOSTING
+
+firebase deploy
