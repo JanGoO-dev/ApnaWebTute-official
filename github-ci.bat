d4ef8d9a09a79a0560eeb4ff3e62ca507b80876c
JanGoO-dev <55313789+JanGoO-dev@users.noreply.github.com>
2021-03-27 15:55:33 +0500

    Testing CI command integration for github push using windows .bat file

--- github-ci.bat
+++ github-ci.bat
@@ -1,1 +1,1 @@
-git add . & set /p msg= "Enter commit message: " & git commit -m "%msg%" & git push origin master & echo %msg%
+git add . & set /p msg=Enter commit message:  & git commit -m "%msg%" & git push origin master & echo %msg%
