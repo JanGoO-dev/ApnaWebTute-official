ad2b4626be0ea6471559b36a22dd4e22809f70c5
JanGoO-dev <55313789+JanGoO-dev@users.noreply.github.com>
2021-03-27 16:24:09 +0500

    Testing if .gitignore will ignore the github-ci.bat file or not

--- github-ci.bat
+++ github-ci.bat
@@ -1,29 +1,11 @@
 @echo off & echo Step 1: PUSH CHANGES TO THE GITHUB REPOSITORY
 
-pause
-
-cls
-
 git add .
 @echo off & set /p msg=Enter commit message:  
 git commit -m "%msg%" & git push origin master & echo %msg%
 
-cls
-
 @echo off & echo CHANGES PUSHED SUCCESSFULLY TO THE GITHUB REPOSITORY
-
-pause
-
-cls
 
 @echo off & echo Step 2: BUILD THE PROJECT FOR PRODUCTION AND DEPLOY IT TO FIREBASE HOSTING
 
-pause
-
-cls
-
 npm run build & firebase deploy
-
-@echo off & echo PRODUCTION VERSION DEPLOYED SUCCESSFULLY TO FIREBASE HOSTING
-
-pause
