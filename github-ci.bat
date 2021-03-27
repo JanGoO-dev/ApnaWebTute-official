a6d9798f7d2e79f8ef86b0330b73390c1240fff7
JanGoO-dev <55313789+JanGoO-dev@users.noreply.github.com>
2021-03-27 16:01:04 +0500

    Testing .....

--- github-ci.bat
+++ github-ci.bat
@@ -1,1 +1,2 @@
-git add . & @echo off & set /p msg=Enter commit message:  & git commit -m "%msg%" & git push origin master & echo %msg%
+git add . & @echo off & set /p msg=Enter commit message:  
+git commit -m "%msg%" & git push origin master & echo %msg%
