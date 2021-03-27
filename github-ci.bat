0b2ebad812f58ae0a07be4be3434b3706736760d
JanGoO-dev <55313789+JanGoO-dev@users.noreply.github.com>
2021-03-27 15:43:53 +0500

    Testing Git commands using windows .bat file

--- github-ci.bat
+++ github-ci.bat
@@ -1,1 +1,1 @@
-git add . & git commit -m 'Testing Git commands using windows .bat file' & git push origin master
+git add . & git commit -m "Testing Git commands using windows .bat file" & git push origin master
