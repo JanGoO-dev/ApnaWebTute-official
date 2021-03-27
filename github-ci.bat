14244d7ec3a6ece598eff0e2b518e33cb0ab2845
JanGoO-dev <55313789+JanGoO-dev@users.noreply.github.com>
2021-03-27 15:50:08 +0500

    Testing Git commands using windows .bat file

--- github-ci.bat
+++ github-ci.bat
@@ -1,1 +1,10 @@
-git add . & git commit -m "Testing Git commands using windows .bat file" & git push origin master
+echo "Initializing the selection of files that has been changed" & echo "." & echo "." & echo "."  & echo "." & echo "." & echo "."
+echo "######################################################################"
+git add .
+echo "All the files with changes have been selected and ready to commit" & echo "." & echo "." & echo "."  & echo "." & echo "." & echo "."
+echo "######################################################################"
+git commit -m "Testing Git commands using windows .bat file"
+echo "Latest Changes have been commited successfully!" & echo "." & echo "." & echo "."  & echo "." & echo "." & echo "."
+echo "######################################################################"
+git push origin master
+echo "Latest Version of your project has been pushed to you github repository" & echo "GitHub: https://github.com/JanGoO-dev/ApnaWebTute-official"
